471e94e36093ffc7f157f908595f4a49059e2400
Hager Abd El Galil <Hager.galil66@gmail.com>
2023-04-12 00:17:52 +0200

    Groups Page

---
+++ views/partials/footer.php
@@ -0,0 +1,7 @@
+<footer class="main-footer">
+    <strong>Copyright &copy; 2022-2023 <a href="">Article Management System</a>.</strong>
+    All rights reserved.
+    <div class="float-right d-none d-sm-inline-block">
+        <b>Version</b> 3.2.0
+    </div>
+</footer>
