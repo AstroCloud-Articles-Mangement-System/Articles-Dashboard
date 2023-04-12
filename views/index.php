f0a87d84040fce422980ceb6e447a6ead56cc11b
Radwa99 <radwa.prof.9@gmail.com>
2023-04-12 23:29:59 +0200

    users view

--- views/index.php
+++ views/index.php
@@ -37,7 +37,7 @@
       <!-- Main content -->
       <section class="content" style="margin-top: 7.5%">
         <div class="container-fluid">
-          <?php include 'pages/groups/index.php'; ?>
+          <?php include 'views/pages/users/index.php'; ?>
         </div>
       </section>
     </div>
