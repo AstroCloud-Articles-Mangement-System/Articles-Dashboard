9a206365cc27d31c2692d3563ed7906e75a76a8f
Hager Abd El Galil <Hager.galil66@gmail.com>
2023-04-13 21:05:07 +0200

    Update Groups Routes

---
+++ controllers/group.php
@@ -0,0 +1,3 @@
+<?php
+
+require 'views/pages/groups/index.php';
--- index.php
+++ index.php
@@ -2,24 +2,10 @@
 require_once('config.php');
 $uri = parse_url($_SERVER['REQUEST_URI'])['path'];
 
-// // Remove the folder path if it exists
-// $uri = str_replace(URL_BASE, '/', $uri);
-
-
-// $uri = $_SERVER['REQUEST_URI'];
-
-// // Remove the base URL path if it exists
-// $uri = str_replace(URL_BASE, '', $uri);
-// die($uri);
-
 $routes = [
     '/' => 'controllers/index.php',
     '/users' => 'controllers/user.php',
-
-
-
-
-
+    '/groups' => 'controllers/group.php',
     '/login' => 'controllers/login.php',
 ];
 
@@ -33,7 +19,7 @@ function routeToController($uri,$routes){
 
 function abort($code=404){
     http_response_code($code);
-    var_dump('not found');
+    var_dump('NOT FOUND');
 }
 
 routeToController($uri,$routes);
