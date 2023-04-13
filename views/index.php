b7d72d6fe0393ca1be0c378d5b45d2c0246ccdde
Hager Abd El Galil <Hager.galil66@gmail.com>
2023-04-14 00:26:21 +0200

    Group Page View

--- views/index.php
+++ views/index.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title> Articles Management System </title>
+    <title> Articles Management System</title>
 
     <link rel="stylesheet"
         href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
