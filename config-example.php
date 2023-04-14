40c5711dcbe8dbca68be5e987ec79810eb8ed21a
Hager Abd El Galil <Hager.galil66@gmail.com>
2023-04-14 20:08:39 +0200

    DB Handler

--- config-example.php
+++ config-example.php
@@ -2,7 +2,7 @@
 define("URL_BASE","/PHPproject/PHP-Project/");
 define("__HOST__","localhost");
 define("__USER__","root");
-define("__PASS__","Gogaahmed66##");
+define("__PASS__","");
 define("__DB__","articles_system");
 define("__RECORDS_PER_PAGE__",20);
 define("__Debug__Mode__",0);
