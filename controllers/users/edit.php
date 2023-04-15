79c4cfeda49c87a00529990c23b5ccfb2cf00605
Radwa99 <radwa.prof.9@gmail.com>
2023-04-16 01:26:37 +0200

    check existance of user

--- controllers/users/edit.php
+++ controllers/users/edit.php
@@ -1,9 +1,16 @@
 <?php
-$page = "user_edit";
-$user_id = intval($_GET['id']); 
 $userData = new User();
-$user = $userData->get_user_by_id($user_id);
-
-$group = new Group();
-$allGroups = $group->get_all_groups();
+$user_id = intval($_GET['id']); 
+if ($userData->check_id_existence($user_id)) {
+    $page = "user_edit";
+    $user = $userData->get_user_by_id($user_id);
+    $group = new Group();
+    $allGroups = $group->get_all_groups();
+}else{
+    $page = "users";
+    $_SESSION['error_message'] = "You can't edit user does not exist in db";
+}
+$allUsers = $userData->get_all_users();
 require 'views/index.php';
+
+
