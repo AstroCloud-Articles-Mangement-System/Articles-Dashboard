a51ce41aec9aaf38b9ec3687a18f6d251c890ac7
Hager Abd El Galil <Hager.galil66@gmail.com>
2023-04-16 21:31:48 +0200

    Update Users CRUD

--- controllers/users/create.php
+++ controllers/users/create.php
@@ -1,6 +1,4 @@
 <?php
-require_once('Models/Group.php');
-
 $page = "userscreate";
 $group = new Group;
 $allGroups = $group->get_all_groups();
--- controllers/users/index.php
+++ controllers/users/index.php
@@ -1,5 +1,4 @@
 <?php
-require_once('Models/User.php');
 $page = "users";
 $user = new User;
 $allUsers = $user->get_all_users();
