283339b2a62ca83a76a7bef1309c225c73227f1d
Mariam-Mokhtar <mariamreda1819@gmail.com>
2023-04-15 18:09:33 -0700

    delete_user

--- controllers/users/delete.php
+++ controllers/users/delete.php
@@ -1,11 +1,6 @@
 <?php
 $user_id = intval($_GET['id']); // cast the id parameter to an integer
-die($user_id);
 $var = new User();
-$user = $var->get_user_by_id($user_id);
-require 'views/index.php';
-
-$page="users";
-$user = new User();
-$allUsers = $user->get_all_users();
-require 'views/index.php';
+$user = $var->delete_user($user_id);
+header("Location: " . $_SERVER['HTTP_REFERER']);
+exit;
