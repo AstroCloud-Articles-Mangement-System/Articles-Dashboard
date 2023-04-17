2ae3899567544aeda7e6fc15e7cdd9e85d58ec57
Hager Abd El Galil <Hager.galil66@gmail.com>
2023-04-17 14:11:43 +0200

    Filter Groups Method

--- controllers/groups/index.php
+++ controllers/groups/index.php
@@ -1,5 +1,11 @@
 <?php
 $page="groups";
 $group = new Group;
-$allGroups = $group->get_all_groups();
-require 'views/index.php';
+
+// if(isset($_POST['searchOnGroup'])){
+    var_dump($_POST);
+    $allGroups = $group->filter_groups($_POST['searchOnGroup']);
+// }else{
+//     $allGroups = $group->get_all_groups();
+// }
+// require 'views/index.php';
