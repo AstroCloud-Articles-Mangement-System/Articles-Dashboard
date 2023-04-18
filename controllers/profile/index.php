4cf02d7d432c16b22a5d20ff7354cbe9a243da7c
Omnia-Goher <omnia.goher9@gmail.com>
2023-04-19 01:08:41 +0200

    profile page

--- controllers/profile/index.php
+++ controllers/profile/index.php
@@ -4,11 +4,11 @@ $article = new Article;
 $page = "profile";
 if (isset($_SESSION['user'])) {
     $email = $_SESSION['user']['email'];
-    $sql = "SELECT * FROM `users` WHERE `user_email` = '$email'";
+    //$sql = "SELECT * FROM `users` WHERE `user_email` = '$email'";
+    $sql = "SELECT u.*,g.group_name FROM `users` u ,`groups` g WHERE u.group_id=g.id && u.user_email = '$email'";
     $loggedInUser = $user->get_users_by_any_sql($sql)[0];
-    $user_id = $loggedInUser['id'];
+    $user_id = intval($loggedInUser['id']);
     $sql = "SELECT * FROM `articles` WHERE `user_id` = $user_id";
-    $user_articles = $article->get_articles_by_any_sql($sql);
-    var_dump($loggedInUser, '***********', $user_articles);
+    $user_articles = $article->get_article_by_any_sql($sql);
 }
-//require 'views/index.php';
+require 'views/index.php';
--- views/partials/sidebar.php
+++ views/partials/sidebar.php
@@ -1,6 +1,6 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
   <!-- Brand Logo -->
-  <a href="" class="brand-link">
+  <a href="/" class="brand-link">
     <img src="../views/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
     <span class="brand-text font-weight-light">Article System</span>
   </a>
@@ -10,14 +10,18 @@
     <!-- Sidebar user panel (optional) -->
     <?php
     if (isset($_SESSION['user'])) {
-      echo '<div class="user-panel mt-3 pb-3 mb-3 d-flex">';
-      echo '<div class="image">';
-      echo '<img src="../views/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">';
-      echo ' </div>';
-      echo '<div class="info">';
-      echo '<a href="/profile" class="d-block">User Name</a>';
-      echo '</div>';
-      echo '</div>';
+      $user = new User;
+      $email = $_SESSION['user']['email'];
+      $sql = "SELECT * FROM `users` WHERE `user_email` = '$email'";
+      $loggedInUser = $user->get_users_by_any_sql($sql)[0];
+      echo '<div class="user-panel mt-3 pb-3 mb-3 d-flex">
+      <div class="image">
+      <img src="../views/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+      </div>
+      <div class="info">
+      <a href="/profile" class="d-block text-capitalize">' . $loggedInUser['user_name'] . '</a>
+      </div>
+      </div>';
     }
     ?>
 
