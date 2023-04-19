fdf1fa84151bf65a66f3fd3fd7190cdc5f8ecc32
rowan <rowantamer5@gmail.com>
2023-04-19 02:18:35 +0200

    edit

--- views/partials/sidebar.php
+++ views/partials/sidebar.php
@@ -1,7 +1,8 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
   <!-- Brand Logo -->
   <a href="/" class="brand-link">
-    <img src="../views/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+    <img src="../views/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
+      style="opacity: .8">
     <span class="brand-text font-weight-light">Article System</span>
   </a>
 
@@ -27,78 +28,30 @@
 
     <!-- SidebarSearch Form -->
     <div class="form-inline mt-4">
-      <div class="input-group" data-widget="sidebar-search">
-        <input class="form-control form-control-sidebar" type="search" placeholder="Search" aria-label="Search">
-        <div class="input-group-append">
-          <button class="btn btn-sidebar">
-            <i class="fas fa-search fa-fw"></i>
-          </button>
-        </div>
-
-        <!-- SidebarSearch Form -->
-        <div class="form-inline">
-          <div class="input-group" data-widget="sidebar-search">
-            <input class="form-control form-control-sidebar" type="search" placeholder="Search" aria-label="Search">
-            <div class="input-group-append">
-              <button class="btn btn-sidebar">
-                <i class="fas fa-search fa-fw"></i>
-              </button>
-            </div>
+      <!-- SidebarSearch Form -->
+      <div class="form-inline">
+        <div class="input-group" data-widget="sidebar-search">
+          <input class="form-control form-control-sidebar" type="search" placeholder="Search" aria-label="Search">
+          <div class="input-group-append">
+            <button class="btn btn-sidebar">
+              <i class="fas fa-search fa-fw"></i>
+            </button>
           </div>
         </div>
-
-        <!-- Sidebar Menu -->
-        <nav class="mt-2">
-          <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-            <li class="nav-item">
-              <a href="/login" class="nav-link">
-                <i class="nav-icon fas fa-table"></i>
-                <p>
-                  Login
-                </p>
-              </a>
-            </li>
-            <?php if ($_SESSION['user']['role'] === 'admin') { ?>
-              <li class="nav-item">
-                <a href="/users" class="nav-link">
-                  <i class="nav-icon fas fa-table"></i>
-                  <p>
-                    Users
-                  </p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="/groups" class="nav-link">
-                  <i class="nav-icon fas fa-table"></i>
-                  <p>
-                    Groups
-                  </p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="/articles" class="nav-link">
-                  <i class="nav-icon fas fa-table"></i>
-                  <p>
-                    Articles
-                  </p>
-                </a>
-              </li>
-            <?php } elseif ($_SESSION['user']['role'] === 'editor') { ?>
-              <li class="nav-item">
-                <a href="/articles" class="nav-link">
-                  <i class="nav-icon fas fa-table"></i>
-                  <p>
-                    Articles
-                  </p>
-                </a>
-              </li>
-            <?php } ?>
-          </ul>
-        </nav>
       </div>
-      <<<<<<< HEAD <!-- Sidebar Menu -->
-        <nav class="mt-2">
-          <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+
+      <!-- Sidebar Menu -->
+      <nav class="mt-2">
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+          <li class="nav-item">
+            <a href="/login" class="nav-link">
+              <i class="nav-icon fas fa-table"></i>
+              <p>
+                Login
+              </p>
+            </a>
+          </li>
+          <?php if ($_SESSION['user']['role'] === 'admin') { ?>
             <li class="nav-item">
               <a href="/users" class="nav-link">
                 <i class="nav-icon fas fa-table"></i>
@@ -123,10 +76,18 @@
                 </p>
               </a>
             </li>
-
-          </ul>
-        </nav>
+          <?php } elseif ($_SESSION['user']['role'] === 'editor') { ?>
+            <li class="nav-item">
+              <a href="/articles" class="nav-link">
+                <i class="nav-icon fas fa-table"></i>
+                <p>
+                  Articles
+                </p>
+              </a>
+            </li>
+          <?php } ?>
+        </ul>
+      </nav>
     </div>
-    =======
-    >>>>>>> 9a8dddd793b528e0890a1df32ea21d2cf233bf1d
+
 </aside>
