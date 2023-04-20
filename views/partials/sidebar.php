fb762ed3236034f1f5cad5a6d3e31b7e202acac3
Hager Abd El Galil <Hager.galil66@gmail.com>
2023-04-20 03:09:18 +0200

    Logo

--- views/partials/sidebar.php
+++ views/partials/sidebar.php
@@ -1,10 +1,11 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="/" class="brand-link">
-        <img src="../views/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
-            style="opacity: .8">
-        <span class="brand-text font-weight-light">Article System</span>
-    </a>
+    <div class="col-12 d-flex flex-column justify-content-center align-items-center">
+        <div>
+            <img src="../views/dist/img/transparent-logo.gif" alt="Logo" class="img-circle" style="width:150px;">
+        </div>
+        <div class="col-12 font-weight-light text-center">Article System</div>
+    </div>
 
     <!-- Sidebar -->
     <div class="sidebar">
